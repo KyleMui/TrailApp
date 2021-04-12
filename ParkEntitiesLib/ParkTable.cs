d3273792217d1e4fbb718705babbf542c592fbbe
KyleMui <44453997+KyleMui@users.noreply.github.com>
2021-04-12 01:13:42 -0400

    Add files via upload

---
+++ ParkEntitiesLib/ParkTable.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+namespace Packt.Shared {
+ public class ParkTable {
+
+public int TrailID {get; set;}
+ public string Park_Name { get; set; }
+ public int Width_ft { get; set; }
+ public string Class { get; set; }
+ public string Surface {get; set;}
+
+ public string Gen_Topog {get; set;}
+
+ public int Difficulty {get; set;}
+ }
+}
