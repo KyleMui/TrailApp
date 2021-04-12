7cf2461f57e9962cc985cdaba54a380292caa751
KyleMui <44453997+KyleMui@users.noreply.github.com>
2021-04-12 01:46:36 -0400

    Add files via upload

---
+++ ParkContextLib/ParkApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace Packt.Shared {
+ public class ParkApp : DbContext {
+ public DbSet<ParkTable> ParkTable { get; set; }
+ public ParkApp(DbContextOptions<ParkApp> options) : base(options) { }
+    protected override void OnModelCreating (
+      ModelBuilder modelBuilder )
+    {
+      base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<ParkTable>()
+        .HasNoKey()
+        .Property(c => c.Park_Name)
+        .IsRequired()
+        .HasMaxLength(30);
+
+    }
+ }
+
+}
